42db0912dfa1e966ad5335e83ebde7b21ae71ae3
sample <niufsd8@gmail.com>
2021-01-26 21:28:22 +0900

    fixed #87

--- src/components/PresenterIcon.tsx
+++ src/components/PresenterIcon.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-
 import { connect } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 
 import { TStore } from "../store";
+import { selectPeer } from "../actions";
 import { Member } from "../types";
 
 import AudienceNumbers from "./AudienceNumbers";
@@ -10,11 +12,75 @@ import AudienceNumbers from "./AudienceNumbers";
 import "./PresenterIcon.css";
 
 interface IProps {
+  selectPeer: any;
+  localPeer?: Peer;
   audiences: Member[];
   presenter?: Member;
 }
 
 class PresenterIcon extends React.PureComponent<IProps> {
+  private _buttonRef = React.createRef<HTMLSpanElement | any>();
+
+  private _pointingTimer = 0;
+  private _isMultiTouchAction = false;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this._onPointingDown = this._onPointingDown.bind(this);
+    this._onPointingUp = this._onPointingUp.bind(this);
+  }
+
+  _onSelectPeerAction(actionFunction: Function) {
+    const { localPeer } = this.props;
+    if (!localPeer) {
+      return;
+    }
+    actionFunction(localPeer.id);
+  }
+
+  _onPointingDown(e: any) {
+    window.clearTimeout(this._pointingTimer);
+
+    if (e.targetTouches?.length > 1) {
+      // Ignore multi touch action.
+      this._isMultiTouchAction = true;
+      return;
+    }
+
+    this._pointingTimer = window.setTimeout(() => {
+      if (e.type === "mousedown" || e.type === "touchstart") {
+        const { localPeer } = this.props;
+        if (!localPeer) {
+          return;
+        }
+        this._onSelectPeerAction(this.props.selectPeer);
+      }
+    }, 500);
+  }
+
+  _onPointingUp(e: any) {
+    window.clearTimeout(this._pointingTimer);
+
+    if (e.targetTouches?.length > 0) {
+      // Still touching by some fingers.
+      return;
+    }
+
+    if (this._isMultiTouchAction) {
+      // Ignore multi touch action.
+    }
+    this._isMultiTouchAction = false;
+  }
+
+  componentDidMount() {
+    const button = this._buttonRef.current;
+    button.addEventListener("mousedown", this._onPointingDown);
+    button.addEventListener("mouseup", this._onPointingUp);
+    button.addEventListener("touchstart", this._onPointingDown);
+    button.addEventListener("touchend", this._onPointingUp);
+  }
+
   render() {
     const { audiences, presenter } = this.props;
     const index = audiences.findIndex(a => a.peerId === presenter?.peerId);
@@ -22,7 +88,7 @@ class PresenterIcon extends React.PureComponent<IProps> {
       "presenter-item " + (presenter ? "" : "presenter-item--no-presenter");
     return (
       <>
-        <span className={className}>
+        <span ref={this._buttonRef} className={className}>
           {presenter ? (
             <img
               src={audiences[index].dataURL}
@@ -38,9 +104,17 @@ class PresenterIcon extends React.PureComponent<IProps> {
 
 const mapStateToProps = (store: TStore) => {
   return {
+    localPeer: store.state.localPeer,
     audiences: store.state.audiences,
     presenter: store.state.presenter,
   };
 };
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<TStore, void, AnyAction>
+) => ({
+  selectPeer: (peerId: string) => {
+    dispatch(selectPeer(peerId));
+  },
+});
 
-export default connect(mapStateToProps)(PresenterIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(PresenterIcon);
